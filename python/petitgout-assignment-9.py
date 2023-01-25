d078a0f4258ab99b8e8c512b41ee0654b313a038
Rea0129 <rea0129@yahoo.com>
2023-01-25 16:31:10 -0700

    adding files

---
+++ python/petitgout-assignment-9.py
@@ -0,0 +1,16 @@
+# GIS- 4007
+# Winter Term
+# Assignment: Python Coding Project - Array
+# March 3, 2020
+# Rachel Petitgout
+
+# Combined all actions into one file, check indicual files for explantion of each code.
+
+wutang = ["Rza", "Gza", "Ol Dirty Bastard and Young Dirty Bastard", "Method Man", "Raekwon", "Ghostface Killah", "Inspecta Deck", "UGod", "Masta Killa", "Cappadonna"]
+print(wutang)
+
+x = len(wutang)
+print(x)
+
+for x in wutang:
+    print(x)
---
+++ python/petitgout-assignment-python-coding-project-length of array.py
@@ -0,0 +1,11 @@
+# GIS- 4007
+# Winter Term
+# Assignment: Python Coding Project - Array
+# March 3, 2020
+# Rachel Petitgout
+
+#This code shows the number of items
+
+wutang = ["Rza", "Gza", "Ol Dirty Bastard and Young Dirty Bastard", "Method Man", "Raekwon", "Ghostface Killah", "Inspecta Deck", "UGod", "Masta Killa", "Cappadonna"]
+x = len(wutang) # length/ number of items in array
+print(x) # print the length
