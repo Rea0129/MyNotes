686086fc325bb8c5e1cdff6cee61412a774ba5df
Rea0129 <rea0129@yahoo.com>
2023-01-25 16:31:10 -0700

    adding files

---
+++ python/petitgout-assignment-python-coding-project-array.py
@@ -0,0 +1,10 @@
+# GIS- 4007
+# Winter Term
+# Assignment: Python Coding Project - Array
+# March 3, 2020
+# Rachel Petitgout
+
+# This code is an array. All items/members are stored in array/wutang
+
+wutang = ["Rza", "Gza", "Ol Dirty Bastard and Young Dirty Bastard", "Method Man", "Raekwon", "Ghostface Killah", "Inspecta Deck", "UGod", "Masta Killa", "Cappadonna"]
+print(wutang) # All items in array will be displayed / printed
---
+++ python/petitgout-assignment-python-coding-project-looping-array.py
@@ -0,0 +1,12 @@
+# GIS- 4007
+# Winter Term
+# Assignment: Python Coding Project - Array
+# March 3, 2020
+# Rachel Petitgout
+
+#This code makes the items in a lopping array
+wutang = ["Rza", "Gza", "Ol Dirty Bastard and Young Dirty Bastard", "Method Man", "Raekwon", "Ghostface Killah", "Inspecta Deck", "UGod", "Masta Killa", "Cappadonna"]
+for x in wutang: # for loop to repeat 
+    print(x) # print the loop
+    
+
